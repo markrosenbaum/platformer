48a1987e69c2bd1adea9d9e2e3df6b74d6977e45
Mark Rosenbaum <57112559+markrosenbaum@users.noreply.github.com>
2025-06-12 14:02:44 +0000

    WFAST and GFADE

--- workspace/src/gamelogic/player/Player.java
+++ workspace/src/gamelogic/player/Player.java
@@ -2,6 +2,9 @@ package gamelogic.player;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.AlphaComposite;
+import java.awt.Composite;
 
 import gameengine.PhysicsObject;
 import gameengine.graphics.MyGraphics;
@@ -9,55 +12,75 @@ import gameengine.hitbox.RectHitbox;
 import gamelogic.Main;
 import gamelogic.level.Level;
 import gamelogic.tiles.Tile;
+import gamelogic.tiles.Water;
+import gamelogic.tiles.Gas;
 
-public class Player extends PhysicsObject{
-	public float walkSpeed = 400;
-	public float jumpPower = 1350;
+public class Player extends PhysicsObject {
+    public float walkSpeed = 400;
+    public float jumpPower = 1350;
+    private boolean isJumping = false;
+    private float opacity = 1.0f;
 
-	private boolean isJumping = false;
+    public Player(float x, float y, Level level) {
+        super(x, y, level.getLevelData().getTileSize(), level.getLevelData().getTileSize(), level);
+        int offset = (int)(level.getLevelData().getTileSize()*0.1);
+        this.hitbox = new RectHitbox(this, offset, offset, width - offset, height - offset);
+    }
 
-	public Player(float x, float y, Level level) {
-	
-		super(x, y, level.getLevelData().getTileSize(), level.getLevelData().getTileSize(), level);
-		int offset =(int)(level.getLevelData().getTileSize()*0.1); //hitbox is offset by 10% of the player size.
-		this.hitbox = new RectHitbox(this, offset,offset, width -offset, height - offset);
-	}
+    @Override
+    public void update(float tslf) {
+        float currentSpeed = walkSpeed;
+        
+        // Check if in water
+        if(isInContact(Water.class)) {
+            currentSpeed *= 1.5f;
+        }
+        
+        // Apply movement
+        movementVector.x = 0;
+        if(PlayerInput.isLeftKeyDown()) {
+            movementVector.x = -currentSpeed;
+        }
+        if(PlayerInput.isRightKeyDown()) {
+            movementVector.x = +currentSpeed;
+        }
+        if(PlayerInput.isJumpKeyDown() && !isJumping) {
+            movementVector.y = -jumpPower;
+            isJumping = true;
+        }
+        
+        // Update opacity based on gas contact
+        opacity = isInContact(Gas.class) ? 0.5f : 1.0f;
+        
+        super.update(tslf);
+        
+        isJumping = true;
+        if(collisionMatrix[BOT] != null) isJumping = false;
+    }
 
-	@Override
-	public void update(float tslf) {
-		super.update(tslf);
-		
-		movementVector.x = 0;
-		if(PlayerInput.isLeftKeyDown()) {
-			movementVector.x = -walkSpeed;
-		}
-		if(PlayerInput.isRightKeyDown()) {
-			movementVector.x = +walkSpeed;
-		}
-		if(PlayerInput.isJumpKeyDown() && !isJumping) {
-			movementVector.y = -jumpPower;
-			isJumping = true;
-		}
-		
-		isJumping = true;
-		if(collisionMatrix[BOT] != null) isJumping = false;
-	}
-
-	@Override
-	public void draw(Graphics g) {
-		g.setColor(Color.YELLOW);
-		MyGraphics.fillRectWithOutline(g, (int)getX(), (int)getY(), width, height);
-		
-		if(Main.DEBUGGING) {
-			for (int i = 0; i < closestMatrix.length; i++) {
-				Tile t = closestMatrix[i];
-				if(t != null) {
-					g.setColor(Color.RED);
-					g.drawRect((int)t.getX(), (int)t.getY(), t.getSize(), t.getSize());
-				}
-			}
-		}
-		
-		hitbox.draw(g);
-	}
+    @Override
+    public void draw(Graphics g) {
+        if(g instanceof Graphics2D) {
+            Graphics2D g2d = (Graphics2D)g;
+            Composite originalComposite = g2d.getComposite();
+            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
+            
+            g.setColor(Color.YELLOW);
+            MyGraphics.fillRectWithOutline(g, (int)getX(), (int)getY(), width, height);
+            
+            if(Main.DEBUGGING) {
+                for (int i = 0; i < closestMatrix.length; i++) {
+                    Tile t = closestMatrix[i];
+                    if(t != null) {
+                        g.setColor(Color.RED);
+                        g.drawRect((int)t.getX(), (int)t.getY(), t.getSize(), t.getSize());
+                    }
+                }
+            }
+            
+            hitbox.draw(g);
+            
+            g2d.setComposite(originalComposite);
+        }
+    }
 }
